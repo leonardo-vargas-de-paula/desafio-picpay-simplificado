61a5526bb7d1d98c8b2a448e0f1d2c749d3be402
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-05 17:37:27 -0400

    fix: add @repository

--- src/main/java/com/example/desafio_picpay_simplificado/repository/UserRepository.java
+++ src/main/java/com/example/desafio_picpay_simplificado/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.example.desafio_picpay_simplificado.repository;
 
 import com.example.desafio_picpay_simplificado.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDocument(String document);
     Optional<User> findUserById(Long id);
