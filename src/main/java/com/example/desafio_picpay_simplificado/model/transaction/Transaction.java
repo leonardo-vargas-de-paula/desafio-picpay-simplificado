301d40d15abc19ea1563713a14224ad7c545c3bf
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-05 17:28:58 -0400

    feat: add transaction model

---
+++ src/main/java/com/example/desafio_picpay_simplificado/model/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package com.example.desafio_picpay_simplificado.model.transaction;
+
+import com.example.desafio_picpay_simplificado.model.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+
+
+}
