6a448ca12979f8e3b970fda5645c4fd83eb39b31
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-06 15:43:39 -0400

    feat: add metodo fromEntity para converter entidades em dtos

--- src/main/java/com/example/desafio_picpay_simplificado/dto/UserDTO.java
+++ src/main/java/com/example/desafio_picpay_simplificado/dto/UserDTO.java
@@ -1,9 +1,22 @@
 package com.example.desafio_picpay_simplificado.dto;
 
+import com.example.desafio_picpay_simplificado.model.user.User;
 import com.example.desafio_picpay_simplificado.model.user.UserType;
 
 import java.math.BigDecimal;
 
 public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password, UserType userType) {
+
+    public static UserDTO fromEntity(User user) {
+        return new UserDTO(
+                user.getFirstName(),
+                user.getLastName(),
+                user.getDocument(),
+                user.getBalance(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getUserType()
+        );
+    }
 
 }
