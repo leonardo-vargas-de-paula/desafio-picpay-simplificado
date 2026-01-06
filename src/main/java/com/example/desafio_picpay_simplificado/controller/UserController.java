709b1d86d3945d7e23efbbcbda135954f0b86baf
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-06 15:42:51 -0400

    feat: add UserController

---
+++ src/main/java/com/example/desafio_picpay_simplificado/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.example.desafio_picpay_simplificado.controller;
+
+import com.example.desafio_picpay_simplificado.dto.UserDTO;
+import com.example.desafio_picpay_simplificado.model.user.User;
+import com.example.desafio_picpay_simplificado.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO){
+        User user = userService.createUser(userDTO);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        UserDTO newDTO = userService.updateUser(id, userDTO);
+        return new ResponseEntity<>(newDTO, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers(){
+        List<UserDTO> users = this.userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        UserDTO user = userService.getUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
