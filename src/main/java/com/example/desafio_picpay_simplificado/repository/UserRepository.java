25a80785be9a7c7d2cc8d1db55e02d6e381dfbb8
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-06 15:43:10 -0400

    feat: add query que resgata todos os usuarios e converte para dtos

--- src/main/java/com/example/desafio_picpay_simplificado/repository/UserRepository.java
+++ src/main/java/com/example/desafio_picpay_simplificado/repository/UserRepository.java
@@ -1,13 +1,20 @@
 package com.example.desafio_picpay_simplificado.repository;
 
+import com.example.desafio_picpay_simplificado.dto.UserDTO;
 import com.example.desafio_picpay_simplificado.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDocument(String document);
     Optional<User> findUserById(Long id);
+
+    @Query("SELECT new com.example.desafio_picpay_simplificado.dto.UserDTO(u.firstName, u.lastName, u.document, u.balance, u.email, u.password, u.userType) " +
+            "FROM users u")
+    List<UserDTO> findAllAsDTO();
 }
