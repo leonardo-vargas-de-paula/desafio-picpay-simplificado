6113d6ba89854fbf6563ed07bb4daf6a06fe1583
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-07 13:28:34 -0400

    feat: add RecursoNaoEncontradoException

---
+++ src/main/java/com/example/desafio_picpay_simplificado/exceptions/RecursoNaoEncontradoException.java
@@ -0,0 +1,14 @@
+package com.example.desafio_picpay_simplificado.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RecursoNaoEncontradoException extends RuntimeException{
+
+    public RecursoNaoEncontradoException(String mensagem){
+        super(mensagem);
+    }
+
+
+}
