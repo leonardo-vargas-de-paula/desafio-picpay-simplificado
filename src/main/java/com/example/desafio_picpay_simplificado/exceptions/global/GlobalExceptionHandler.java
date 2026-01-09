f0d67e611f876ae7da2487ef6a08342194033b04
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-09 17:06:06 -0400

    feat: adiciona erro para campos de user

--- src/main/java/com/example/desafio_picpay_simplificado/exceptions/global/GlobalExceptionHandler.java
+++ src/main/java/com/example/desafio_picpay_simplificado/exceptions/global/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.example.desafio_picpay_simplificado.exceptions.global;
 
 import com.example.desafio_picpay_simplificado.exceptions.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -95,6 +97,20 @@ public class GlobalExceptionHandler {
         body.put("status", HttpStatus.UNAUTHORIZED.value());
         body.put("message", ex.getMessage());
         return new ResponseEntity<>(body, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        body.put("errors", errors);
+        body.put("message", "Erro de validação nos campos enviados");
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
 }
