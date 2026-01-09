6618a6e66b0eca66aff000e9647449abd92ed37d
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-09 15:53:30 -0400

    feat: add save user

--- src/main/java/com/example/desafio_picpay_simplificado/repository/UserRepository.java
+++ src/main/java/com/example/desafio_picpay_simplificado/repository/UserRepository.java
@@ -14,6 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDocument(String document);
     Optional<User> findUserById(Long id);
     Optional<User> findUserByEmail(String email);
+    void saveUser(User user);
     @Query("SELECT new com.example.desafio_picpay_simplificado.dto.UserDTO(u.firstName, u.lastName, u.document, u.balance, u.email, u.password, u.userType) " +
             "FROM users u")
     List<UserDTO> findAllAsDTO();
