33425339497b009cbb77c744e88031b247af0707
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-30 18:54:02 -0400

    chore: remove codigo repetitivo

--- src/main/java/com/example/desafio_picpay_simplificado/dto/UserDTOResponse.java
+++ src/main/java/com/example/desafio_picpay_simplificado/dto/UserDTOResponse.java
@@ -7,26 +7,4 @@ import java.math.BigDecimal;
 
 public record UserDTOResponse (String firstName, String lastName, String document, BigDecimal balance, String email, UserType userType) {
 
-    public static UserDTOResponse fromEntity(User user) {
-        return new UserDTOResponse(
-                user.getFirstName(),
-                user.getLastName(),
-                user.getDocument(),
-                user.getBalance(),
-                user.getEmail(),
-                user.getUserType()
-        );
-    }
-
-    public static UserDTOResponse fromDTO(UserDTO user) {
-        return new UserDTOResponse(
-                user.firstName(),
-                user.lastName(),
-                user.document(),
-                user.balance(),
-                user.email(),
-                user.userType()
-        );
-    }
-
 }
